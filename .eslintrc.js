21c004c09b546388a6b817d1b6b49a00b4a2742b
Fu Diwei <fudiwei@sina.com>
2020-12-16 16:57:00 +0800

    feat: 调整 prettier 尾逗号配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,48 @@
 module.exports = {
-	env: {
-		node: true
-	},
-	extends: [
-		'plugin:vue/essential',
-		'eslint:recommended',
-		'@vue/typescript/recommended',
-		'@vue/prettier',
-		'@vue/prettier/@typescript-eslint'
-	],
-	overrides: [
-		{
-			files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-			env: {
-				mocha: true
-			}
-		}
-	],
-	parser: 'vue-eslint-parser',
-	parserOptions: {
-		ecmaVersion: 2020,
-		parser: '@typescript-eslint/parser',
-		sourceType: 'module'
-	},
-	plugins: [
-		'@typescript-eslint',
-		'html',
-		'prettier',
-		'vue'
-	],
-	root: true,
-	rules: {
-		'indent': ['error', 4],
-		'linebreak-style': ['error', 'windows'],
-		"max-len": ['warn', 120],
-		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'prettier/prettier': ['error', {
-			usePrettierrc: true
-		}],
-		'quotes': ['error', 'single'],
-		'semi': ['error', 'always'],
-		'vue/html-closing-bracket-spacing': ['error', {
-			selfClosingTag: 'always'
-		}],
-		'vue/no-unused-components': 'warn',
-		'vue/*': 'off',
-		'@typescript-eslint/ban-ts-ignore': 'off',
-		'@typescript-eslint/indent': ['error', 4, { VariableDeclarator: 4, SwitchCase: 1 }],
-		'@typescript-eslint/no-non-null-assertion': 'off'
-	}
-}
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint'
+    ],
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            env: {
+                mocha: true
+            }
+        }
+    ],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+    },
+    plugins: [
+        '@typescript-eslint',
+        'html',
+        'prettier',
+        'vue'
+    ],
+    root: true,
+    rules: {
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'windows'],
+        "max-len": ['warn', 120],
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'prettier/prettier': ['warn', { trailingComma: 'none' }],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'vue/html-closing-bracket-spacing': ['warn', { selfClosingTag: 'always' }],
+        'vue/no-unused-components': 'warn',
+        '@typescript-eslint/ban-ts-ignore': 'warn',
+        '@typescript-eslint/indent': ['error', 4, { VariableDeclarator: 4, SwitchCase: 1 }],
+        '@typescript-eslint/no-non-null-assertion': 'off'
+    }
+};
