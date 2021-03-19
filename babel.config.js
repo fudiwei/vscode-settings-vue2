bea117454c28a01489dbe86060bade548f4524fe
Fu Diwei <fudiwei@sina.com>
2021-03-19 10:03:57 +0800

    feat: edit babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     presets: ['@vue/cli-plugin-babel/preset'],
-    plugins: ['@babel/plugin-proposal-optional-chaining']
-}
+    plugins: [
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-transform-async-to-generator',
+        '@babel/plugin-transform-regenerator',
+        '@babel/plugin-transform-runtime'
+    ]
+};
